c50518de48b083378e2982a1d11d4cf2aa1bd94c
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-06-06 10:44:38 +0300

    Init

---
+++ app/src/main/java/com/example/android/hometask1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.hometask1;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private TextView textView1;
+    private TextView textView2;
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            swap();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView1 = findViewById(R.id.text1);
+        textView2 = findViewById(R.id.text2);
+        textView1.setOnClickListener(clickListener);
+        textView2.setOnClickListener(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swap();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        swap();
+    }
+
+    private void swap(){
+        CharSequence tmp = textView1.getText();
+        textView1.setText(textView2.getText());
+        textView2.setText(tmp);
+
+        ColorDrawable cd1 = (ColorDrawable) textView1.getBackground();
+        ColorDrawable cd2 = (ColorDrawable) textView2.getBackground();
+        int firstColor = cd1.getColor();
+        int secondColor = cd2.getColor();
+        textView1.setBackgroundColor(secondColor);
+        textView2.setBackgroundColor(firstColor);
+    }
+}
