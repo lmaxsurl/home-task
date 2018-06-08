16ac3bdfb1b60e1f9ea6d83cf01b3aae3d57c7f4
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-06-09 00:41:35 +0300

    add main activity

--- app/src/main/java/com/example/android/hometask1/MainActivity.java
+++ app/src/main/java/com/example/android/hometask1/MainActivity.java
@@ -1,57 +1,33 @@
 package com.example.android.hometask1;
 
-import android.graphics.drawable.ColorDrawable;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button;
-    private TextView textView1;
-    private TextView textView2;
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            swap();
-        }
-    };
+    private final int READY_TASKS = 1;
+    private final int FIRST_TASK = 0;
+    private Button[] buttons = new Button[READY_TASKS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        textView1 = findViewById(R.id.text1);
-        textView2 = findViewById(R.id.text2);
-        textView1.setOnClickListener(clickListener);
-        textView2.setOnClickListener(this);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swap();
-            }
-        });
+        buttons[FIRST_TASK] = findViewById(R.id.ht1_button);  //ht1 - home task 1
+       /* buttons[1] = findViewById(R.id.ht2_button);
+        buttons[2] = findViewById(R.id.ht3_button);*/
     }
+
 
     @Override
     public void onClick(View view) {
-        swap();
-    }
-
-    private void swap(){
-        CharSequence tmp = textView1.getText();
-        textView1.setText(textView2.getText());
-        textView2.setText(tmp);
-
-        ColorDrawable cd1 = (ColorDrawable) textView1.getBackground();
-        ColorDrawable cd2 = (ColorDrawable) textView2.getBackground();
-        int firstColor = cd1.getColor();
-        int secondColor = cd2.getColor();
-        textView1.setBackgroundColor(secondColor);
-        textView2.setBackgroundColor(firstColor);
+        switch (view){
+            case buttons[FIRST_TASK]: ht1Activity.start(this);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/hometask1/ht1Activity.java
@@ -0,0 +1,63 @@
+package com.example.android.hometask1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ht1Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private TextView textView1;
+    private TextView textView2;
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            swap();
+        }
+    };
+
+    public static void start(Activity activity){
+        Intent intent = new Intent(activity, ht1Activity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ht1);
+        button = findViewById(R.id.button);
+        textView1 = findViewById(R.id.text1);
+        textView2 = findViewById(R.id.text2);
+        textView1.setOnClickListener(clickListener);
+        textView2.setOnClickListener(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swap();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        swap();
+    }
+
+    private void swap(){
+        CharSequence tmp = textView1.getText();
+        textView1.setText(textView2.getText());
+        textView2.setText(tmp);
+
+        ColorDrawable cd1 = (ColorDrawable) textView1.getBackground();
+        ColorDrawable cd2 = (ColorDrawable) textView2.getBackground();
+        int firstColor = cd1.getColor();
+        int secondColor = cd2.getColor();
+        textView1.setBackgroundColor(secondColor);
+        textView2.setBackgroundColor(firstColor);
+    }
+}
