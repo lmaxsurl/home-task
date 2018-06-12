b605863c0daa31cc76d5fb4ddf2506826c7535cd
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-06-12 21:27:41 +0300

    home task 3 without rounded corner

--- app/src/main/java/com/example/android/hometask1/MainActivity.java
+++ app/src/main/java/com/example/android/hometask1/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.android.hometask1;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.squareup.leakcanary.LeakCanary;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button1;
-    private Button button2;
+    private Button ht1Button;
+    private Button ht2Button;
+    private Button ht3Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.ht1_button);
-        button1.setOnClickListener(this);
+        ht1Button = findViewById(R.id.ht1_button);
+        ht1Button.setOnClickListener(this);
 
-        button2 = findViewById(R.id.ht2_button);
-        button2.setOnClickListener(this);
+        ht2Button = findViewById(R.id.ht2_button);
+        ht2Button.setOnClickListener(this);
+
+        ht3Button = findViewById(R.id.ht3_button);
+        ht3Button.setOnClickListener(this);
     }
 
 
@@ -31,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.ht1_button: ht1Activity.start(this);  //ht1 - home task 1
                 break;
             case R.id.ht2_button: ht2Activity.start(this);
+                break;
+            case R.id.ht3_button: ht3Activity.start(this);
                 break;
         }
     }
---
+++ app/src/main/java/com/example/android/hometask1/ht3Activity.java
@@ -0,0 +1,48 @@
+package com.example.android.hometask1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+public class ht3Activity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private ImageView imageView;
+    private String imageURL = "http://bm.img.com.ua/nxs/img/prikol/images/large/1/2/308321_879389.jpg";
+
+    public static void start(Activity activity){
+        Intent intent = new Intent(activity, ht3Activity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ht3);
+
+        editText = findViewById(R.id.ht3_editText);
+        button = findViewById(R.id.ht3_DownloadButton);
+        imageView = findViewById(R.id.ht3_imageView);
+
+        editText.setText(imageURL);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Picasso.get().load(editText.getText().toString()).into(imageView);
+            }
+        });
+
+    }
+
+
+
+}
